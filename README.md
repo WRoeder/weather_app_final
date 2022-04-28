b3c54c72f63b630be8d5fd2b3dd6fd4a8824a784
WRoeder <77704216+WRoeder@users.noreply.github.com>
2022-04-27 21:26:56 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# weather_app_final
+ Final project weather app utilizing a weather api. 
---
+++ android/app/src/main/kotlin/com/wroeder/com/weather_app_final/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.wroeder.com.weather_app_final
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
